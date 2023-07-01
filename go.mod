f64d3ff3f8a6731d352af5515e42682fcc98d10f
hd4y2t <hd4y2t@gmail.com>
2023-07-01 20:52:53 +0700

    first

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/hd4y2t/app_say_hello
+
+go 1.20
+
+require github.com/hd4y2t/go-say-hello v1.0.0 // indirect
---
+++ main.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+	"fmt"
+
+	go_say_hello "github.com/hd4y2t/go-say-hello"
+)
+
+func main() {
+	fmt.Println(go_say_hello.SayHello())
+}
